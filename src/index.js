1ed79abef8c8c2e6dedab018cdc14d3f0ed6eff3
francescafr <hello@francesca.fyi>
2022-12-07 20:36:15 -0800

    Completed wave 4, maybe 6 too

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ let weatherBox = null;
 let body = null;
 let textBox = null;
 let viewCity = null;
+let getTempButton = null;
+let resetButton = null;
+let setButton = null;
 
 const loadControls = () => {
   state.city = document.getElementById('city');
@@ -24,6 +27,9 @@ const loadControls = () => {
   body = document.querySelector('body');
   textBox = document.getElementById('city');
   viewCity = document.getElementById('view-city');
+  getTempButton = document.getElementById('current-temp');
+  resetButton = document.getElementById('reset-city');
+  setButton = document.getElementById('set-city');
 };
 
 const tempColor = () => {
@@ -62,11 +68,43 @@ const updateCity = () => {
   viewCity.textContent = state.city;
 }
 
+const resetCity = () => {
+  state.city = 'Dallol';
+  viewCity.textContent = state.city;
+  textBox.value = state.city;
+  updateTemp();
+}
+const updateTemp = () => {
+  // const lat = null;
+  // const lon = null;
+  axios.get('http://127.0.0.1:5000/location', { params: { 'q': state.city } })
+    .then((response) => {
+      const lat = response.data[0].lat;
+      const lon = response.data[0].lon;
+      axios.get('http://127.0.0.1:5000/weather', { params: { 'lat': lat, 'lon': lon } })
+        .then((response) => {
+          const tempK = response.data.main.temp;
+          state.temp = Math.floor(((tempK - 273) * (9 / 5)) + 32)
+          displayTemp.textContent = `${state.temp}`;
+          tempColor();
+        })
+        .except((error) => {
+          console.log("Error: Could not get Weather")
+        })
+    })
+    .except((error) => {
+      console.log("Error: COuld not get Lat Lon")
+    })
+}
+
 const registerEventHandlers = () => {
   loadControls();
   plusButton.addEventListener('click', increaseTemp);
   minusButton.addEventListener('click', decreaseTemp);
   textBox.addEventListener('input', updateCity);
+  getTempButton.addEventListener("click", updateTemp);
+  resetButton.addEventListener("click", resetCity);
+  setButton.addEventListener("click", updateTemp);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
