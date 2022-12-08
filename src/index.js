ff071c23d6e55ca63a753dc3b62cbda0c3cfd644
Erika Sha <erikaasha05@gmail.com>
2022-12-07 21:55:42 -0500

    completed wave 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
 'use strict';
+
+const state = {
+  city: 'Dallol',
+  temp: 48,
+  skyType: 'Sunny',
+};
+
+let plusButton = null;
+let displayTemp = null;
+let minusButton = null;
+let weatherBox = null;
+let body = null;
+
+const loadControls = () => {
+  state.city = document.getElementById('city');
+  state.skyType = document.getElementById('skyType');
+  plusButton = document.getElementById('plus');
+  displayTemp = document.getElementById('temp');
+  minusButton = document.getElementById('minus');
+  weatherBox = document.getElementById('weather-box');
+  body = document.querySelector('body');
+};
+
+const tempColor = () => {
+  if (state.temp >= 80) {
+    weatherBox.style.backgroundColor = 'red';
+    body.style.backgroundImage = "url('./assets/sunny.webp')";
+  } else if (state.temp >= 70) {
+    weatherBox.style.backgroundColor = 'orange';
+    body.style.backgroundImage = "url('./assets/spring.jpeg')";
+  } else if (state.temp >= 60) {
+    weatherBox.style.backgroundColor = 'yellow';
+    body.style.backgroundImage = "url('./assets/autumn.jpeg')";
+  } else if (state.temp >= 50) {
+    weatherBox.style.backgroundColor = 'green';
+    body.style.backgroundImage = "url('./assets/snowy.jpg')";
+  } else if (state.temp < 50) {
+    weatherBox.style.backgroundColor = 'teal';
+    body.style.backgroundImage = "url('./assets/snowy.jpg')";
+  }
+};
+
+const increaseTemp = () => {
+  state.temp += 1;
+  displayTemp.textContent = `${state.temp}`;
+  tempColor();
+};
+
+const decreaseTemp = () => {
+  state.temp -= 1;
+  displayTemp.textContent = `${state.temp}`;
+  tempColor();
+};
+
+const registerEventHandlers = () => {
+  loadControls();
+  plusButton.addEventListener('click', increaseTemp);
+  minusButton.addEventListener('click', decreaseTemp);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
